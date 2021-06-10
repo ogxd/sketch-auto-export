82400b34512db65ba767133cc44ae75d83b7e1e7
Olivier Giniaux <oginiaux@gmail.com>
2021-06-10 21:29:09 +0200

    Now working 🎉

--- src/export.js
+++ src/export.js
@@ -1,4 +1,5 @@
 import sketch from 'sketch'
+import dialog from '@skpm/dialog'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
@@ -6,21 +7,23 @@ export default function() {
   const doc = sketch.getSelectedDocument()
   const selectedLayers = doc.selectedLayers
   const selectedCount = selectedLayers.length
+  
+  var output = dialog.showOpenDialogSync({
+    properties: [ 'openDirectory'], buttonLabel: 'Exporter ici', title: "Séléctionnez le dossier d'export"
+  })
 
-  const options = { scales: '1, 2', formats: 'jpg', overwriting: false }
+  console.log('Exporting to : ' + output)
 
-  //Object.keys(selectedLayers).forEach((prop)=> console.log(prop));
-  //sketch.export(doc.pages)
+  output = output + '/'
 
-  console.log('----')
+  const options = { scales: '1, 2', formats: 'jpg', overwriting: true, output: output }
 
   var k = selectedLayers.layers;
 
   for (var p of k) {
     //console.log(JSON.stringify(p, null, 4));
     sketch.export(p, options)
-
-    NSFileManager.defaultManager().moveItemAtPath_toPath_error('/Users/oritross/Documents/Sketch Exports/' + p.name + '@2x.jpg', '/Users/oritross/Documents/Sketch Exports/' + p.name + '_x2.jpg', nil)
+    NSFileManager.defaultManager().moveItemAtPath_toPath_error(output + p.name + '@2x.jpg', output + p.name + '_x2.jpg', nil)
   }
 
   if (selectedCount === 0) {
